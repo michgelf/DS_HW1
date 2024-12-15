10792f53e33f737755553d2cb04259e9a2072539
michgelf <mgelfand@campus.technion.ac.il>
2024-12-16 01:10:18 +0200

    Add FarmEntities.h

---
+++ FarmEntities.h
@@ -0,0 +1,28 @@
+#ifndef HORSE_H
+#define HORSE_H
+
+#include "AVLTree.h"
+
+struct Herd;
+
+struct Horse {
+    int id;
+    int speed;
+    Horse* leader;
+    Herd* herd;
+    int followTime;
+    int joinHerdTime;
+    int pathId;
+
+    Horse(int id, int speed) : id(id), speed(speed), leader(nullptr), herd(nullptr), followTime(-1),
+                               joinHerdTime(-1), pathId(0) {}
+
+};
+
+struct Herd {
+    int id;
+    AVLTree<Horse*> horses;
+};
+
+
+#endif
